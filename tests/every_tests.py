d6788d8b2718cd63e1e76eea8baf41ae5324031e
Alan Grover <awgrover@gmail.com>
2020-12-07 18:37:24 -0500

    add tests

---
+++ tests/every_tests.py
@@ -0,0 +1,165 @@
+import unittest
+import sys, os
+_here = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(_here)
+from every.every import Every
+import time, math
+
+class PeriodAndDurationTests(unittest.TestCase):
+    def setUp(self):
+        # we are going to be tricky, and make it here, so it's start is now
+        # when we test it, other tests may have run, using up its time!
+        self.start_tester = Every(0.05)
+
+    def test0Sanity_monotonic(self):
+        # make sure our assumptions hold
+
+        now = time.monotonic()
+        dumywork = 0
+        for x in range(1,100):
+            dumywork += 1
+        finished = time.monotonic()
+        assert now != finished, "Time actually passes according to monotonic()"
+        assert finished - now < 0.005, "But, we can get a lot of work done in 0.05 (actual %s)" % (finished-now)
+
+    def testSimpleEvery(self):
+        tenth = Every(0.1)
+        start=time.monotonic()
+        finished = None
+        while( not finished and time.monotonic() - start < 1):
+            if tenth():
+                finished = time.monotonic()
+
+        assert math.isclose(finished-start, 0.0, rel_tol=0.1, abs_tol=0.001), "1st period is instantly, actually %s" % (finished-start)
+
+        start=time.monotonic()
+        finished = None
+        while( not finished and time.monotonic() - start < 1):
+            if tenth():
+                finished = time.monotonic()
+
+        assert math.isclose(finished-start, 0.1, rel_tol=0.1, abs_tol=0.0), "Elapsed ~0.1, actually %s" % (finished-start)
+
+    def testEveries(self):
+        # because Every takes time, we are going to test a bunch of things in parallel
+        # I had to tune math.isclose( ... rel_tol=x ) to match my systems performance. yuk
+        
+        everies = []
+
+        # periodics
+        # we cause these to NOT fire instantly ( .start() ), so no 1st 0 duration:
+        everies.append( (Every(0.1), 0.1, 0.1) )
+        everies.append( (Every(0.1, 0.15), 0.1, 0.15) )
+        # don't get too long, it takes the total just to run the tests!
+        everies.append( (Every(0.1, 0.15, 0.2, 0.1), 0.1, 0.15, 0.2, 0.1) )
+
+        # durations, put a fake -1, which makes us keep trying for a firing, and which we handle special below
+        everies.append( ( Every(0.1, 0), 0.1, -1 ))
+        everies.append( ( Every(0.2, 0.15, 0.1, 0), 0.2, 0.15, 0.1, -1 ))
+
+        start = time.monotonic()
+        results = []
+        done = False
+
+        # avoid previous tests delaying the start (start is at construction time)
+        for an_every, *want in everies:
+            # Also, this causes it to NOT fire instantly
+            an_every.start()
+
+        while not done and time.monotonic()-start < 1.0:
+            i = 0
+            done = True
+            elapsed = time.monotonic()
+            for an_every, *want in everies:
+                if len( results ) <= i:
+                    results.append( [ start ] ) # an extra for deltas!
+                if len(results[i]) < len(want) + 1: # extra, and the rest
+                    done = False
+                    if an_every():
+                        # just store the time, we'll "delta" it later
+                        results[ i ].append( elapsed )
+                        #print("  hit", i,elapsed-start,elapsed)
+                i += 1
+
+        for res_i,raw_result in enumerate(results):
+
+            last = raw_result[0]
+            #print("last", last)
+            result = []
+            for elapsed in raw_result[1:]:
+                result.append( elapsed - last )
+                #print("  delta %s ela %s last %s" % (elapsed - last, elapsed, last) )
+                last = elapsed
+            # for -1 being the "no more" marker, for duration/timers, make result have it
+            if everies[res_i][-1] == -1:
+                result.append(-1)
+            #print( "[%s] %s SHOULD= %s" % (res_i,everies[res_i][1],result) )
+            #print( "  raw",raw_result)
+            
+            an_every, *want = everies[res_i]
+
+            self.do_intervals_match( '[%s]' % res_i, want, result )
+
+    def do_intervals_match(self, prefix_msg, actual, want ):
+        #print( "  e[]", want)
+        isclose = []
+        is_ok = True
+
+        for i,an_actual in enumerate(actual):
+            if len(want) > i:
+                isclose.append( math.isclose(an_actual, want[i], rel_tol=0.1, abs_tol=0.001) )
+                is_ok = is_ok and isclose[-1]
+
+        assert is_ok and len(actual) == len(want), "Expected %s to be ~ %s, but got %s (%s)" % (prefix_msg, want, actual, isclose)
+
+    def testLastIsStart(self):
+        tester = Every(0.05)
+
+        # again, instantly starts
+        now = time.monotonic() - 0.05 
+        assert math.isclose(tester.last-now, 0, rel_tol=0.01, abs_tol=0.001),"After constructor, expected .last == now, saw %s != %s " % (tester.last,now)
+        time.sleep(0.05) # make things different
+        tester.start()
+        # But, .start() won't fire till first duration
+        now = time.monotonic()
+        # (test vs 0, because we really want to test digits of significance)
+        assert math.isclose(tester.last-now, 0, rel_tol=0.01, abs_tol=0.001),"After .start(), expected .last == now, saw %s != %s " % (tester.last,now)
+
+    def testStart(self):
+        test_start = time.monotonic()
+
+        hit_at = None
+        while( time.monotonic() - test_start < 1):
+            if self.start_tester():
+                hit_at = time.monotonic()
+        assert hit_at, "Expected to expire at least once!" # sanity
+
+    def testSetInterval(self):
+        test_start = time.monotonic()
+
+        unchanged = Every(0.05)
+        changed = Every(0.05)
+        changed.interval = 0.1
+
+        hit_at = {
+            'unchanged' : [],
+            'changed' : [],
+            }
+
+
+        # let the slowest happen twice
+        while( time.monotonic() - test_start < 0.24):
+            if unchanged():
+                #print("<<hit unchagned", time.monotonic() - test_start )
+                hit_at['unchanged'].append( time.monotonic() - test_start )
+            if changed():
+                #print("<<hit chagned", time.monotonic() - test_start )
+                hit_at['changed'].append( time.monotonic() - test_start )
+
+        self.do_intervals_match( 'unchanged', hit_at['unchanged'], [ 0.0, 0.05, 0.1, 0.15, 0.2 ] )
+        print( "ci", changed.interval)
+        self.do_intervals_match( 'changed', hit_at['changed'], [ 0.0, 0.1, 0.2] )
+        
+
+if __name__ == "__main__":
+    unittest.main() # run all tests
